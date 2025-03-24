edfbacf7f9946bc4946fdab285139df71a331161
jaba_mira <artem.lesnikov.2006@mail.ru>
2025-03-25 02:13:47 +0700

    listViews

--- Spectr/Pages/AdministratorPage.xaml.cs
+++ Spectr/Pages/AdministratorPage.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Contract = Spectr.Data.Contract;
 
 namespace Spectr
 {
@@ -29,8 +30,9 @@ namespace Spectr
         public AdministratorPage(Administrator administrator)
         {
             Administrator = administrator;
-            Debug.WriteLine(administrator.AdministratorLogin);
+         
             InitializeComponent();
+            LabelLogin.Content = administrator.AdministratorLogin;
             LoadContracts();
         }
         private void LoadContracts()
@@ -41,16 +43,24 @@ namespace Spectr
                     .Include(c => c.Customer)
                     .Include(c => c.Administrator)
                     .Include(c => c.Areas)
+                        .ThenInclude(a => a.AreaCoordinates) // Включаем координаты зон
+                    .Include(c => c.Areas)
                         .ThenInclude(a => a.Profiles)
-                            .ThenInclude(p => p.Pickets)
+                            .ThenInclude(p => p.ProfileCoordinates) // Включаем координаты профилей
+                    .Include(c => c.Areas)
+                        .ThenInclude(a => a.Profiles)
+                            .ThenInclude(p => p.Pickets) // Включаем пикеты
+                                .ThenInclude(o=>o.Operator)
                     .ToList();
+
                 treeView.ItemsSource = contracts;
-                Debug.WriteLine(contracts[0].Areas.ToString());
             }
-               
+
+
+
 
             // Устанавливаем контракты в ItemsSource для TreeView
-         
+
         }
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
@@ -99,13 +109,92 @@ namespace Spectr
 
         }
 
-        private void treeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (treeView.SelectedItem is TreeViewItem selectedItem)
-            {
-                infoLabel.Content = selectedItem.Header.ToString();
-            }
-
+          
         }
+
+        private void treeView_SelectedItemChanged_1(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (treeView.SelectedItem is Contract selectedContract)
+            {
+                listViewArea.ItemsSource = selectedContract.Areas;
+                ResetVisibility();
+                listViewArea.Visibility = Visibility.Visible;
+                listViewArea.Items.Refresh();
+
+                 List<Operator> allOperators = selectedContract.Areas
+                .SelectMany(a => a.Profiles)
+                .SelectMany(p => p.Pickets)
+                .Select(pk => pk.Operator)
+                .Distinct()
+                .ToList();
+
+                listViewOperators.ItemsSource = allOperators;
+                listViewOperators.Items.Refresh();
+            }
+            if (treeView.SelectedItem is Area selecteArea)
+            {
+                listViewProfile.ItemsSource = selecteArea.Profiles;
+                listViewAreaCoordinates.ItemsSource = selecteArea.AreaCoordinates;
+
+                ResetVisibility();
+                listViewProfile.Visibility = Visibility.Visible;
+                listViewAreaCoordinates.Visibility = Visibility.Visible;
+                listViewProfile.Items.Refresh();
+                listViewAreaCoordinates.Items.Refresh();
+
+                List<Operator> allOperators = selecteArea.Profiles
+                .SelectMany(p => p.Pickets)
+                .Select(pk => pk.Operator)
+                .Distinct()
+                .ToList();
+
+                listViewOperators.ItemsSource = allOperators;
+                listViewOperators.Items.Refresh();
+            }
+            if (treeView.SelectedItem is Profile selectedProfile)
+            {
+                
+                listViewPickets.ItemsSource = selectedProfile.Pickets;
+                listViewProfileCoordinates.ItemsSource = selectedProfile.ProfileCoordinates;
+                
+                ResetVisibility();
+                listViewPickets.Visibility = Visibility.Visible;
+                listViewProfileCoordinates.Visibility = Visibility.Visible;
+               
+                listViewPickets.Items.Refresh();
+                listViewProfileCoordinates.Items.Refresh();
+
+                List<Operator> allOperators = selectedProfile.Pickets
+              .Select(pk => pk.Operator)
+              .Distinct()
+              .ToList();
+
+                listViewOperators.ItemsSource = allOperators;
+                listViewOperators.Items.Refresh();
+            }
+            if (treeView.SelectedItem is Picket selectedPicket)
+            {
+                ResetVisibility();
+                listViewPickets.Items.Refresh();
+
+                List<Operator> allOperators = new List<Operator> { selectedPicket.Operator };
+
+                listViewOperators.ItemsSource = allOperators;
+                listViewOperators.Items.Refresh();
+            }
+            Debug.WriteLine(1);
+           
+        }
+        private void ResetVisibility()
+        {
+            listViewArea.Visibility = Visibility.Collapsed;
+            listViewPickets.Visibility = Visibility.Collapsed;
+            listViewProfile.Visibility = Visibility.Collapsed;
+            listViewProfileCoordinates.Visibility = Visibility.Collapsed;
+            listViewAreaCoordinates.Visibility = Visibility.Collapsed;
+        }
+
     }
 }
